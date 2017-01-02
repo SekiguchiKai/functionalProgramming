ea24b59bf7ec797ed6cbd77b787a0a780b4b912a
SekiguchiKai <sekiguchikai@SekiguchiKai-1.local>
2017-01-02 11:17:26 +0900

    argumentsオブジェクトを使った例を作成

--- highHierarchyFunc.js
+++ highHierarchyFunc.js
@@ -7,3 +7,19 @@ function greet(name) {
     return (name + "こんにちは");
 }
 alert(sayName(greet));
+
+// 高階関数2
+function calcTotal() {
+    var total = 0;
+    for (var i = 0; i < arguments.length; i++) {
+        total += arguments[i];
+    }
+
+    return total;
+}
+
+function calcTax(calc) {
+    return calc * 1.08;
+}
+
+alert(calcTax(calcTotal(100, 200, 300)));
