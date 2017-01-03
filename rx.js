2aa4e852ed3505b94be1129fc3e46ba36d5f0af5
SekiguchiKai <sekiguchikai@SekiguchiKai-1.local>
2017-01-03 20:55:35 +0900

    rx.jsにmapを追加

--- rx.js
+++ rx.js
@@ -6,8 +6,16 @@ let stream = rx.Observable.fromArray(array);
 stream.subscribe((proLang) => console.log(proLang));
 
 
-// 偶数だけ処理する
+// 【filter】偶数だけ処理する
 let numArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 let numStream = rx.Observable.fromArray(numArray);
 
 numStream.filter(num => num % 2 === 0).subscribe(filtered => console.log(filtered));
+
+// 【map】入力を返還する
+numStream.map((num) => {
+    let alphabet = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    return alphabet[num];
+})
+    .subscribe(alphabet => console.log(alphabet));
+
